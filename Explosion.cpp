de73260ea9b0f94006ec3622732937eb0ef5c95f
AkkarinJung <ths40834@ths.hal.ac.jp>
2026-09-14 17:44:23 +0900

    upload new number sprite

--- Explosion.cpp
+++ Explosion.cpp
@@ -58,7 +58,7 @@ void Explosion::Init()
 
     TexMetadata metadata;
     ScratchImage image;
-    LoadFromWICFile(L"asset\\texture\\Explosion.png", WIC_FLAGS_NONE, &metadata, image);
+    LoadFromWICFile(L"asset\\texture\\Smoke.png", WIC_FLAGS_NONE, &metadata, image);
     CreateShaderResourceView(Renderer::GetDevice(), image.GetImages(),
         image.GetImageCount(), metadata, &m_Texture);
     assert(m_Texture);
